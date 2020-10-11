a86445411d7c0684c38941acf19328c649b84db5
Sachin.B.Lohar <69034411+sachinl0har@users.noreply.github.com>
2020-10-11 23:38:26 +0530

    Update Bubble_sort.c

--- Bubble_sort.c
+++ Bubble_sort.c
@@ -1,65 +1,41 @@
 #include <stdio.h>
-
-int bubble_sort(char *a, int n);
-
-int main(void) {
-    char a[10];
-    int n = sizeof a / sizeof a[10];
+int compcount=0;
+void main()
+{
+    int a[20],i,n;
+    printf("How many numbers:");
+    scanf("%d",&n);
+    printf("\nEnter the unsorted elements:");
+    accept(a,n);
+    bubbleSort(a,n);
+    printf("\nThe sorted output is:");
+    display(a,n);
+    printf("\nTotal numberss of comparisons = %d", compcount);
+}
+void bubbleSort(int a[20],int n)
+{
+    int i,temp,pass;
+    for (pass=1;pass<n;pass++)
+    for (i=0;i<=n-pass-1;i++)
+    {
+        compcount++;
+        if(a[i]>a[i+1])
+        {
+            temp=a[i];
+            a[i]=a[i+1];
+            a[i+1]=temp;
+        }
+    }
+}
+void accept (int a[], int n)
+{
     int i;
-    char inname;
-    char outname;
-
-    printf("Enter input name: ");
-    scanf("%s", &inname);
-    printf("Enter output name: ");
-    scanf("%s", &outname);
-
-    FILE *in, *out;
-
-    out = fopen(&outname, "w");
-
-    if ((in = fopen(&inname, "r")) == NULL) {
-        printf("File not found\n");
-
-    }
-
-
-    else {
-        for (int i = 0; i < 10; i++)
-        {
-            fscanf(in, "%s ", &a[i]);
-        }
-
-        bubble_sort(a, n);
-
-        for (i = 0; i < 10; i++) {
-            printf("%s\n", a[i]);
-            fprintf(out, "%s\n", a[i]);
-        }
-    }
-
-        fclose(in);
-        fclose(out);
-        return 0;
-    }
-
-
-int bubble_sort(char *a, int n) {
-    int i, j;
-    char temp;
-
-        for (j = 1; j<n; j++)
-        {
-            for (i = 0; i<n - j; i++)
-            {
-                if ((int)a[i] >= (int)a[i + 1])
-                {
-                    temp = a[i];
-                    a[i] = a[i + 1];
-                    a[i + 1] = temp;
-
-            }
-        }
-    }
-    return a[i];
+    for (i=0;i<n;i++)
+    scanf("%d",&a[i]);
+}
+void display (int a[],int n)
+{
+    int i;
+    for (i=0;i<n;i++)
+    printf("%d \t",a[i]);
 }
